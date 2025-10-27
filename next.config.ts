a400c4c7b07e3f98086a5df9c4eb0168e6009d57
Tiny <chrishall11993@gmail.com>
2025-10-27 22:30:10 +0000

    fix: next.config + force dynamic render

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 // next.config.ts
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // ⚠️ DO NOT set: output: "export"
 };
-
 export default nextConfig;
