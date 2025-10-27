6fcfee2cb26aec51aae851f3bc3ca1f56560ba80
Tiny <chrishall11993@gmail.com>
2025-10-27 22:18:48 +0000

    changed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 "use client";
+"use client";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { useMemo, useState } from "react";
 import TrackCard from "@/components/TrackCard";
 import PlayerBar, { Track } from "@/components/PlayerBar";
