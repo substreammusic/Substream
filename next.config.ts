8e3b1dffd11cd22c025eecf8c16419c754356906
Tiny <chrishall11993@gmail.com>
2025-10-27 21:44:09 +0000

    changed next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // ⚠️ DO NOT set: output: "export"
 };
 
 export default nextConfig;
